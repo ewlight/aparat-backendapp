19500679279f238bd918c2acddfe65184774915c
Eka Widiantara <eka.widiantara@go-jek.com>
2018-10-21 03:57:08 +0700

    [EKA] Implement Save User Data

--- application/controllers/Aparat.php
+++ application/controllers/Aparat.php
@@ -1,9 +1,50 @@
 <?php
 require APPPATH . '/libraries/REST_Controller.php';
 
+
+
 class Aparat extends \Restserver\Libraries\REST_Controller {
-    public function index_get()
-    {
-        $this->response("hello");
+
+    public function __construct() {
+        parent::__construct();
+        $this->CI =& get_instance();
+        $this->load->model("peserta");
+
     }
+
+    public function wrapper($code,$message,  $data) {
+        return array('status' => $code, 'message' => $message, 'data' => $data);
+    }
+
+    public function login_post() {
+        $fbid = $this->post("fbid");
+        $fbtoken = $this->post("fbtoken");
+        $email = $this->post("email");
+        $name = $this->post("name");
+        $profilepict = $this->post("profilepict");
+
+        $result = $this->peserta->savePeserta($fbid, $fbtoken, $email, $name, $profilepict);
+
+        if($result == 1) {
+            $this->response($this->wrapper(200, "Welcome New User - $name",""), 200);
+        } else {
+            $this->response($this->wrapper(200, "Welcome Back $name",""), 200);
+        }
+
+    }
+
+    public function profile_get() {
+        $fbid = $this->get("fbid");
+        $result = $this->peserta->getPesertaDetail($fbid);
+        if(empty($result) || is_null($result)) {
+            $this->response($this->wrapper(404,"Data Not Found",""));
+        } else {
+            $this->response($this->wrapper(200, "Success", $result));
+        }
+    }
+
+    public function index_get() {
+        $this->response($this->wrapper(200,"Testing Okey",""));
+    }
+
 }
---
+++ application/models/Peserta.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ewlight
+ * Date: 20/10/18
+ * Time: 10:36 PM
+ */
+
+class Peserta extends CI_Model
+{
+    public function __construct() {
+        parent::__construct();
+
+    }
+
+    public function getPesertaDetail($id) {
+        $sqlget = "select fbid, fbtoken, email, name, profilepict from peserta where fbid = $id";
+        return $this->db->query($sqlget)->result();
+    }
+
+    public function savePeserta($fbid, $fbtoken, $email, $name, $profilepict){
+        $checkSavedPeserta = "select id from peserta where fbid = '$fbid'";
+        $numPeserta = $this->db->query($checkSavedPeserta)->num_rows();
+        if($numPeserta == 0) {
+            $savedSql = "insert into peserta values(null,'$fbid','$fbtoken','$email','$name','$profilepict')";
+            $this->db->query($savedSql);
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+}
